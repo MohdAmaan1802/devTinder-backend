32a87aeb3cdca4c13b736e8f6277360a400b01e2
MohdAmaan1802 <mohdamaan1802@gmail.com>
2025-05-29 17:57:37 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -16,6 +16,45 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+//get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  // try {
+  //   const user = await User.findOne({ emailId: userEmail });
+  //   if (!user) {
+  //     res.status(404).send("User not found");
+  //   } else {
+  //     res.send(user);
+  //   }
+  // } catch (err) {
+  //   console.error("Error fetching user:", err);
+  //   res.status(500).send("Error fetching user");
+  // }
+
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    console.error("Error fetching users:", err);
+    res.status(500).send("Error fetching users");
+  }
+});
+
+//feed API - GET /feed -get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    console.error("Error fetching users:", err);
+    res.status(500).send("Error fetching users");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("DB connected successfully");
