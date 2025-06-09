b6dc66359818789fa70e989c12bae51b307a3c0d
MohdAmaan1802 <mohdamaan1802@gmail.com>
2025-06-09 18:02:36 +0530

    api update

--- src/app.js
+++ src/app.js
@@ -55,6 +55,35 @@ app.get("/feed", async (req, res) => {
   }
 });
 
+//delete a user
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.send("User deleted successfully");
+  } catch (err) {
+    console.error("Error deleting user:", err);
+    res.status(500).send("Error deleting user");
+  }
+});
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+    });
+    console.log("User updated:", user);
+    res.send("User updated successfully");
+  } catch (err) {
+    console.error("Error updating user:", err);
+    res.status(500).send("Error updating user");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("DB connected successfully");
