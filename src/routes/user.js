f845a076c8cee30b3a82cbb61dd76f7f469e13df
MohdAmaan1802 <mohdamaan1802@gmail.com>
2025-08-25 18:33:35 +0530

    API_FEED

--- src/routes/user.js
+++ src/routes/user.js
@@ -62,6 +62,12 @@ userRouter.get("/feed", userAuth, async (req, res) => {
     // his connections
     // already sent connection requests
     // ignored people
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit; //max limit is 50
+    const skip = (page - 1) * limit;
+
     const loggedInUser = req.user;
     const connectionRequests = await ConnectionRequest.find({
       $or: [{ formUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
@@ -78,7 +84,10 @@ userRouter.get("/feed", userAuth, async (req, res) => {
         { _id: { $nin: Array.from(hideUsersFromFeed) } },
         { _id: { $ne: loggedInUser._id } },
       ],
-    }).select(USER_SAFE_DATA);
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
 
     res.json({ data: users });
   } catch (error) {
